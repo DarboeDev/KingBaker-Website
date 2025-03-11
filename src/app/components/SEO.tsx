4565d4c07f3804d6a5e0c7a2839e9589a4404add
Muhammed Darboe <125799918+DarboeDev@users.noreply.github.com>
2025-03-11 15:40:31 +0000

    update seo

--- src/app/components/SEO.tsx
+++ src/app/components/SEO.tsx
@@ -13,6 +13,20 @@ const SEO = ({
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@kingbakers" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="description" content="king bakers gambia" />
+      <meta name="keywords" content="king bakers, bakery, supermarket, cafe" />
+      <meta name="author" content="King Bakers" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="google" content="nositelinkssearchbox" />
+      <meta name="google" content="notranslate" />
     </Head>
   );
 };
